4a1f59c1b1f7546a6e8cbd33b30a39da97463846
jcserv <jarrod.servilla@gmail.com>
2024-08-06 05:05:40 -0400

    tweak: change data model

--- get-visit-count-lambda/api/v1.py
+++ get-visit-count-lambda/api/v1.py
@@ -5,7 +5,7 @@ import boto3
 from boto3.dynamodb.conditions import Key
 from typing import Dict
 
-router = APIRouter()
+router = APIRouter(prefix="/view-counts")
 
 REGION=os.environ.get("AWS_REGION")
 QUEUE_URL=os.environ.get("QUEUE_URL")
@@ -26,16 +26,18 @@ class ViewCountResponse(BaseModel):
 async def get_view_count(user_id: str) -> Dict[str, str]:
     try:
         response = table.query(
-            KeyConditionExpression=Key('PK').eq(user_id)
+            KeyConditionExpression=Key('PK').eq(user_id),
+            Limit=250,
+            ConsistentRead=True,
         )
         items = response.get('Items', [])
 
         view_count = 0
-        if len(items) > 0 :
+        if len(items) > 0:
             user_data = items[0]
-            view_count = user_data.get('view_count', 0)
+            view_count = user_data.get('count', 0) + len(items) - 1
 
-        send_message_sqs(user_id)
+        send_message_sqs(user_id, view_count)
 
         return {
             "schemaVersion": 1,
@@ -44,14 +46,15 @@ async def get_view_count(user_id: str) -> Dict[str, str]:
             "color": "blue"
         }
     except HTTPException as e:
-        raise e  # Re-raise the HTTPException so it retains its status code
+        raise e 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-def send_message_sqs(user_id: str):
+def send_message_sqs(user_id: str, count: int):
     try:
         message = {
-            'userId': user_id
+            'userId': user_id,
+            'count': count,
         }
         
         response = sqs.send_message(
