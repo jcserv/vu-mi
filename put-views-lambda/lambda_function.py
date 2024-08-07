90278861610b2488056239838ab45c21aed952e7
jcserv <jarrod.servilla@gmail.com>
2024-08-06 20:18:32 -0400

    feat: implement batch update count lambda

--- put-views-lambda/lambda_function.py
+++ put-views-lambda/lambda_function.py
@@ -3,7 +3,7 @@ import json
 import re
 import os
 from aws_lambda_typing import context as context_, events
-from datetime import datetime, timezone
+from datetime import datetime, timedelta, timezone
 
 REGION=os.environ.get("AWS_REGION")
 VIEW_COUNTS_TABLE_NAME=os.environ.get("VIEW_COUNTS_TABLE_NAME")
@@ -11,7 +11,7 @@ VIEW_COUNTS_TABLE_NAME=os.environ.get("VIEW_COUNTS_TABLE_NAME")
 dynamodb = boto3.resource('dynamodb', region_name=REGION)
 table = dynamodb.Table(VIEW_COUNTS_TABLE_NAME)
 
-def lambda_handler(event, context: context_.Context) -> None:
+def lambda_handler(event: events.SQSEvent, context: context_.Context) -> None:
     for message in event["Records"]:
         process_message(convert_str_to_json(message["body"]))
 
@@ -43,12 +43,12 @@ def initialize_user(user_id):
 
 def put_view(user_id):
     curr_time = datetime.now(timezone.utc)
-    five_minutes_in_seconds = (5 * 60) - (now.minute % 5) * 60 - now.second
+    five_minutes_in_seconds = (5 * 60) - (curr_time.minute % 5) * 60 - curr_time.second
     try:
         item = {
             "PK": user_id,
             "SK": f'VIEW#{curr_time.isoformat()}',
-            "ttl": int((curr_time + timedelta(seconds=seconds_to_add)).timestamp()),
+            "ttl": int((curr_time + timedelta(seconds=five_minutes_in_seconds)).timestamp()),
         }
         response = table.put_item(
             Item=item,
